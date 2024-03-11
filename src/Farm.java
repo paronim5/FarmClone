3f1d08c4f59303f1c25ea3bc2124bc160885002c
kosov <kosov@spsejecna.cz>
2024-03-11 08:06:33 +0100

    added class Sheep added new methods in class Farm and add List of animal and list of Flowers

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,12 @@
+import java.util.List;
+
 public class Farm {
+    List<Animal> animals;
+    List<Flower>flowers;
+
+public void zasadit(){}
+public void zavlazovat (){}
+public void sklizet (){}
 
 
 
