ec24aa8ff872dbc6541101a650d5078a8c1086bc
kosov <kosov@spsejecna.cz>
2024-03-11 09:08:33 +0100

    added class Sheep added new methods in class Farm and add List of animal and list of Flowers add  pozemek = 100; change in class Flower nedded area to String add new methods for farmer add new classes for flower add new classes for animal add getters and setters add method pohladit add methods kupovat a prodat but they dont work

--- src/Farm.java
+++ src/Farm.java
@@ -2,20 +2,34 @@ import java.util.List;
 
 public class Farm {
 
-   private List<Animal> animals;
-   private List<Flower> flowers;
-   private int pozemek = 100;
+    List<Animal> animals;
+    List<Flower> flowers;
+    int pozemek = 100;
+
+   public Farm() {
+   }
    //for flower
 
+
 public void zasadit(){
+
 }
 public void zavlazovat (){}
 public void sklizet (){}
    // for animal
-   public void kupovat(){}
-   public void prodat(){}
-   public void pohladit(Animal animal){
-      System.out.println("pohladili jste "+ animal.name);
+   public void kupovat(Animal animal){
+         animals.add(animal);
+         System.out.println("koupili jste "+ animal.type + " named "+ animal.name);
+
+   }
+   public  void prodat(Animal animal){
+if (animals.contains(animal)){
+   animals.remove(animal);
+   System.out.println("prodali jste "+ animal.type+ " named "+ animal.name );
+}
+   }
+   public static void pohladit(Animal animal){
+      System.out.println("pohladili jste "+ animal.type+" named " + animal.name);
    }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Farm f1 = new Farm();
+     Animal a1= new Frog("frog","petr",Size.SMALL,"none");
+Farm.pohladit(a1);
+f1.kupovat(a1);
+f1.prodat(a1);
     }
 }
